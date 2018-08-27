c017ac63d9fdc814b97e8e4ebaae45ad972d110c
Tariq <tariq@tsspltd.com>
2018-08-27 11:20:05 +0530

    completed large hero block and into content block

---
+++ gulp/task/styles.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var rename = require('gulp-rename');
+var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
+
+gulp.task('styles', function() {
+  return gulp
+    .src('./app/assets/styles/*.scss')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
