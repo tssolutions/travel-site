ede62e389418ce5704f03203cb4ebae852d882de
Tariq <tariq@tsspltd.com>
2018-09-07 18:31:42 +0530

    scroll section complete

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/mobilemenu';
+import RelevalOnScroll from './modules/revealonscroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+new RelevalOnScroll($('.feature-item'), '85%');
+new RelevalOnScroll($('.testimonial'), '60%');
