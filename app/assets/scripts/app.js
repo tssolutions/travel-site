44f173f74be8c2354626e627f824f196243c38ce
Tariq <tariq@tsspltd.com>
2018-09-08 12:45:10 +0530

    completed modal overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,9 +2,11 @@ import MobileMenu from './modules/mobilemenu';
 import RelevalOnScroll from './modules/revealonscroll';
 import $ from 'jquery';
 import StickyHeader from './modules/sticky-header';
+import Modal from './modules/modal';
 
 var mobileMenu = new MobileMenu();
 
 new RelevalOnScroll($('.feature-item'), '85%');
 new RelevalOnScroll($('.testimonial'), '60%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
