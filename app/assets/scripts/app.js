ad4e20fab90d2d987f80598fd75ddf71537dd0f8
Tariq <tariq@tsspltd.com>
2018-09-08 10:30:17 +0530

    sticky header done

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/mobilemenu';
 import RelevalOnScroll from './modules/revealonscroll';
 import $ from 'jquery';
+import StickyHeader from './modules/sticky-header';
 
 var mobileMenu = new MobileMenu();
 
 new RelevalOnScroll($('.feature-item'), '85%');
 new RelevalOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
